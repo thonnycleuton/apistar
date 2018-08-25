81d5bb3fba2a7d69dacec0ae559940d819932452
tonynascimento <thonnycleuton@gmail.com>
2018-08-25 01:29:45 -0300

    adaptacao para apistar .5

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 # Django specific settings
 import os
-import typing
-
-from apistar import App, Route, types, validators
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_star.settings")
+
+from apistar import App, Route
+from django_orm.views import *
 
 # Ensure settings are read
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-
-# Your application specific
-from django_orm.models import User as UserDB
-
-
-class User(types.Type):
-    user_identity = validators.String(max_length=50)
-    full_name = validators.String(max_length=100)
-    status = validators.String(enum=['active', 'inactive'])
 
 
 def welcome(name=None):
@@ -27,26 +17,15 @@ def welcome(name=None):
     return {'message': 'Welcome to API Star, %s!' % name}
 
 
-def list_users() -> typing.List[User]:
-    try:
-        queryset = UserDB.objects.all()
-        return [User(record) for record in queryset]
-    except Exception as e:
-        print(e)
-
-
-def create_users() -> typing.List[User]:
-    queryset = None
-    return [User(record) for record in queryset]
-
-
 routes = [
     Route('/', method='GET', handler=welcome),
     Route('/users/', method='GET', handler=list_users),
-    Route('/users/', method='POST', handler=create_users)
+    Route('/users/', method='POST', handler=create_users),
+    Route('/products/', method='GET', handler=list_products),
+    Route('/products/', method='POST', handler=create_product),
 ]
 
 app = App(routes=routes)
 
 if __name__ == '__main__':
-    app.serve('127.0.0.1', 5000, use_debugger=True, use_reloader=True)
+    app.serve('127.0.0.1', 8000, use_debugger=True, use_reloader=True)
--- django_orm/models.py
+++ django_orm/models.py
@@ -9,7 +9,21 @@ STATUS_CHOICES = (
 
 
 class User(models.Model):
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_identity = models.CharField(max_length=50)
     full_name = models.CharField(max_length=100)
     status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+
+
+class Product(models.Model):
+    SIZE_CHOICES = (
+        ('small', 'small'),
+        ('medium', 'medium'),
+        ('large', 'large'),
+    )
+
+    name = models.CharField(max_length=100)
+    in_stock = models.BooleanField()
+    rating = models.PositiveSmallIntegerField(null=True, blank=True)
+    size = models.CharField(max_length=100, choices=SIZE_CHOICES, null=True, blank=True)
